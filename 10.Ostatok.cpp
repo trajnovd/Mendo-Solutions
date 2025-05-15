4b0dae936a91804f51b9087407ab8ea7e0e31d1a
Darko Trajanov <167031667+trajnovd@users.noreply.github.com>
2025-05-16 01:33:24 +0200

    Update 10.Ostatok.cpp

--- 10.Ostatok.cpp
+++ 10.Ostatok.cpp
@@ -1,1 +1,18 @@
-// Ostatok
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <set>
+using namespace std;
+ 
+int main(){
+    int a[12];
+    set<int> s;
+    for (int i = 0; i < 10; i++) {
+        cin >> a[i];
+        s.insert(a[i]%42);
+    }
+   cout << s.size();
+}
