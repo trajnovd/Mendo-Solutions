d9b27c1241e8e06fbb3c57e6fb609ed9300d51a0
Darko Trajanov <167031667+trajnovd@users.noreply.github.com>
2025-05-16 01:33:58 +0200

    Update 11.Trajko.cpp

--- 11.Trajko.cpp
+++ 11.Trajko.cpp
@@ -1,1 +1,35 @@
-// Trajko
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <set>
+#include <cmath>
+ 
+using namespace std;
+ 
+int main(){
+int n,m;
+cin >> n >> m;
+vector<int> v;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+    sort(v.rbegin(), v.rend());
+    int res = 0;
+    for (int i = 0; i < n;) {
+        if(i+m<n){
+            res = v[i]*2+res;
+            i = i+m;
+        }
+        else{
+            res = res + v[i]*2;
+            i = i+(n-i);
+        }
+ 
+    }
+    cout << res;
+}
