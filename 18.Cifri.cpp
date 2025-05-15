b5d4e1ce04637f78c009e40df2727894ace11623
Darko Trajanov <167031667+trajnovd@users.noreply.github.com>
2025-05-16 01:37:20 +0200

    Update 18.Cifri.cpp

--- 18.Cifri.cpp
+++ 18.Cifri.cpp
@@ -1,1 +1,26 @@
-// Cifri
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+ 
+ 
+int main() {
+string s;
+cin >>s;
+int k;
+cin >> k;
+int n = s.size();
+string ans = "";
+    for (int bitmask = 0; bitmask < (1 << n); ++bitmask) {
+        string tmp = "";
+        for (int i = 0; i < n; ++i) {
+            if(bitmask & (1<<i)){
+                tmp+=s[i];
+            }
+        }
+        if(tmp.size() == n-k){
+            ans = max(ans,tmp);
+        }
+    }
+cout << ans << endl ;
+}
