429491c8b5b206985a261d5a1ef8b47dd44097f6
Darko Trajanov <167031667+trajnovd@users.noreply.github.com>
2025-05-16 01:35:16 +0200

    Update 13.Anagram.cpp

--- 13.Anagram.cpp
+++ 13.Anagram.cpp
@@ -1,1 +1,26 @@
-// Anagram
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <set>
+#include <cmath>
+ 
+using namespace std;
+ 
+int main(){
+int n;
+cin >>n;
+int brojac =0;
+    for (int i = 0; i < n; i++) {
+        string s,s1;
+        cin>> s >> s1;
+        sort(s.begin(), s.end());
+        sort(s1.begin(),s1.end());
+        if(s == s1){
+            brojac++;
+        }
+    }
+    cout << brojac;
+}
