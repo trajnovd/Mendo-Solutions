d9cdc48130af6fbc1c9f5f5be049d4a69334aa9a
Darko Trajanov <167031667+trajnovd@users.noreply.github.com>
2025-05-16 02:04:55 +0200

    Update 29.Shoping.cpp

--- 29.Shoping.cpp
+++ 29.Shoping.cpp
@@ -1,1 +1,28 @@
-// Shoping
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+  
+int main() {
+    int n,d;
+    cin >> n >> d;
+    double popust = (double)(100 -d)/100;
+    double arr[1000];
+    for(int i = 0;i<n;i++){
+        cin >> arr[i];
+    }
+    sort(arr,arr+n);
+    double sum =0;
+    int p = n-(n/3);
+    for (int i = 0; i < n; i++) {
+        if(p>i) {
+            sum += arr[i];
+        }
+  
+        if(i>=p){
+            sum+= arr[i]*popust;
+        }
+    }
+   cout  << fixed << setprecision(2) << sum;
+}
