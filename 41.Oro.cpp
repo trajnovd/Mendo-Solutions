2189e6313e69e567d36c8c92b2a34c9ab3fabae5
Darko Trajanov <167031667+trajnovd@users.noreply.github.com>
2025-05-16 02:12:48 +0200

    Update 41.Oro.cpp

--- 41.Oro.cpp
+++ 41.Oro.cpp
@@ -1,1 +1,35 @@
-// Oro
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+using namespace  std;
+int p,m;
+int n;
+int a[1005];
+int memo[1005][55];
+int f(int k,int i){
+    if(k<0 || k>m){
+        return -1;
+    }
+    if(i == n ){
+        return k;
+    }
+    if(memo[k][i] != -1){
+        return memo[k][i];
+    }
+    int left= -1;
+    int right= -1;
+    left = f(k-a[i],i+1);
+    right = f(k+a[i],i+1);
+    int res = -1;
+    res = max(left,right);
+    return memo[k][i]= res;
+}
+int main() {
+    cin >> p >> m >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    memset(memo, -1, sizeof memo);
+    cout << f(p,0);
+}
