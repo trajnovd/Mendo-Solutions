52d1a313ac6112fa2bb2625a0eb706cdf2fd78c3
Darko Trajanov <167031667+trajnovd@users.noreply.github.com>
2025-05-16 02:06:09 +0200

    Update 31.Kutii.cpp

--- 31.Kutii.cpp
+++ 31.Kutii.cpp
@@ -1,1 +1,32 @@
-// Kutii
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+ 
+int main()
+{
+int n,m;
+cin >> n >> m;
+int predmeti[n];
+int kutii[m];
+ 
+for(int i = 0;i<n;i++){
+    cin >> predmeti[i];
+}
+for(int i =0;i<m;i++){
+    cin >> kutii[i];
+}
+ 
+int res = 0;
+ 
+for(int i = 0;i<n;i++){
+    for(int j = 0;j<m;j++){
+        if(predmeti[i]<= kutii[j]){
+            res++;
+            kutii[j] = -1;
+            break;
+        }
+    }
+}
+cout << res;
+}
