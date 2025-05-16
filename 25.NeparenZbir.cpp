bec6f0be4e73136d22e2e89dcb6141ace4af187a
Darko Trajanov <167031667+trajnovd@users.noreply.github.com>
2025-05-16 02:03:12 +0200

    Update 25.NeparenZbir.cpp

--- 25.NeparenZbir.cpp
+++ 25.NeparenZbir.cpp
@@ -1,1 +1,14 @@
-// NeparenZbir
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int a,b;
+    cin >> a  >> b;
+    int suma = 0;
+    for(int i =a; i<=b; i++){
+        if(i%2 == 1){
+            suma += i;
+        }
+    }
+    cout << suma;
+}
