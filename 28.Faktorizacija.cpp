8f41616a57976698c376c634fca164e26da74e47
Darko Trajanov <167031667+trajnovd@users.noreply.github.com>
2025-05-16 02:04:34 +0200

    Update 28.Faktorizacija.cpp

--- 28.Faktorizacija.cpp
+++ 28.Faktorizacija.cpp
@@ -1,1 +1,37 @@
-// Faktorizacija
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+    int n;
+    cin >> n;
+  
+    bool prv = true;
+  
+    for (int i=2; i<=n; i++)
+    {
+        if (n%i == 0)
+        {
+            int stepen = 0;
+  
+            while (n%i == 0)
+            {
+                n /= i;
+                stepen++;
+            }
+  
+            if (prv)
+            {
+                cout << "(" << i << "^" << stepen << ")";
+  
+                prv = false;
+            }
+            else
+            {
+                cout << "*(" << i << "^" << stepen << ")";
+            }
+        }
+    }
+  
+    return 0;
+}
